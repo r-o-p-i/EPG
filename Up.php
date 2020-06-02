afc570b47872890968454cc91eba56288cf760d0
ropi <ropi_k@mail.ru>
2020-06-02 11:11:25 +0300

    post ssl

--- Up.php
+++ Up.php
@@ -307,7 +307,8 @@ class Up
     if ($head) {
       curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
     }
-
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $tost);
